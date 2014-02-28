4d453d0889847158f2ef555ed5c07ccdb1053175
Jakub Sygnowski <sygnowski@gmail.com>
2014-02-28 22:24:28 +0100

    scan list

--- src/eu/rotato/diceplushackaton/LearningAPIActivity.java
+++ src/eu/rotato/diceplushackaton/LearningAPIActivity.java
@@ -1,18 +1,56 @@
 package eu.rotato.diceplushackaton;
 
-import android.os.Bundle;
+import us.dicepl.android.sdk.BluetoothManipulator;
+import us.dicepl.android.sdk.DiceController;
+import us.dicepl.android.sdk.DiceScanningListener;
+import us.dicepl.android.sdk.Die;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class LearningAPIActivity extends Activity {
 
 	private static final int[] developerKey = new int[] {0x83, 0xed, 0x60, 0x0e, 0x5d, 0x31, 0x8f, 0xe7};
+	private static final String TAG = "DICEPlus";	
+	private Die dice;
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
+    DiceScanningListener scanningListener = new DiceScanningListener() {
+        @Override
+        public void onNewDie(Die die) {
+            Log.d(TAG, "New DICE+ found");
+            dice = die;
+            DiceController.connect(dice);
+        }
+
+        @Override
+        public void onScanStarted() {
+        	Toast.makeText(getApplication(), "scan started", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onScanFailed() {
+            Log.d(TAG, "Scan Failed");
+            BluetoothManipulator.startScan();
+        }
+
+        @Override
+        public void onScanFinished() {
+            Log.d(TAG, "Scan Finished");
+
+            if(dice == null) {
+                BluetoothManipulator.startScan();
+            }
+        }
+    };
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
