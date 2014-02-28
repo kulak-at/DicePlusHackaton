f7b4aac13355f983b7f405c83db07ac6e88a4101
Jakub Sygnowski <sygnowski@gmail.com>
2014-02-28 22:16:53 +0100

    starting - learning api team

---
+++ src/eu/rotato/diceplushackaton/LearningAPIActivity.java
@@ -0,0 +1,24 @@
+package eu.rotato.diceplushackaton;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class LearningAPIActivity extends Activity {
+
+	private static final int[] developerKey = new int[] {0x83, 0xed, 0x60, 0x0e, 0x5d, 0x31, 0x8f, 0xe7};
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
